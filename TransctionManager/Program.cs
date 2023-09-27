be60c04b1963a28ca6283a44da7b9da6bb2ac1ac
guipa <guipa@GUIPA>
2023-09-27 10:54:36 +0100

    Adicionar arquivos de projeto.

---
+++ TransctionManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TransctionManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
