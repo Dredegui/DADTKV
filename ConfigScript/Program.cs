730fb07a6b5e645214715a0d803f22e04f99afb4
JoseCutileiro <jos3ndo@gmail.com>
2023-10-01 09:55:26 +0100

    feat: add parse for config script

--- ConfigScript/Program.cs
+++ ConfigScript/Program.cs
@@ -1,37 +1,135 @@
-﻿namespace ConfigScript
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Schema;
+using System.Text.RegularExpressions;
+
+namespace ConfigScript
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static string PATH = Process.GetCurrentProcess().MainModule.FileName;
+        static string SOLUTION_PATH = getSolutionPath();
+        static string TM_PATH = SOLUTION_PATH + "\\TransactionManager\\bin\\Debug\\net6.0\\TransactionManager.exe";
+        static string CLI_PATH = SOLUTION_PATH + "\\Client\\bin\\Debug\\net6.0\\Client.exe";
+        static string LM_PATH = SOLUTION_PATH + "\\LeaseManager\\bin\\Debug\\net6.0\\LeaseManager.exe";
+        
+        public static Tuple<string,string> splitStr(string str)
         {
-            if (args.Length != 0)
+            string ret_1 = "";
+            string ret_2 = "";
+            bool s = false;
+            int size = str.Length;
+
+            for (int i = 0; i < size; i++)
             {
-                if (args[0] == "1")
+                if (str[i] == ' ' && !s)
                 {
-                    AlternatePathOfExecution();
+                    s = true;
                 }
-                //add other options here and below              
+                else
+                {
+                    if (!s)
+                    {
+                        ret_1 += str[i];
+                    }
+                    else
+                    {
+                        ret_2 += str[i];
+                    }
+                }
             }
-            else
+
+            return Tuple.Create(ret_1, ret_2);
+        }
+        
+        public static void startNewProcess(Tuple<string,string> split, string line)
+        {
+            // GET NAME
+            string name = split.Item1;
+            line = split.Item2;
+
+            // WHICH PROCESS
+            split = splitStr(line);
+            string PDcp = split.Item1;
+            string inp = split.Item2;
+
+            string path_exe = null;
+            if (PDcp == "T")
             {
-                NormalPathOfExectution();
+                path_exe = TM_PATH;
+            }
+            if (PDcp == "L")
+            {
+                path_exe = LM_PATH;
+            }
+            if (PDcp == "C")
+            {
+                path_exe = CLI_PATH;
+            }
+            if (path_exe != null)
+            {
+                Process.Start(path_exe, name + " " + inp);
             }
         }
 
-
-        private static void NormalPathOfExectution()
+        public static void lineBehaviour(string line)
         {
-            Console.WriteLine("Doing something here");
-            //need one of these for each additional console window
-            System.Diagnostics.Process.Start("ConfigScript.exe", "1");
-            Console.ReadLine();
 
+            Tuple<string,string> split = splitStr(line);
+            string func = split.Item1;
+            line = split.Item2;
+            split = splitStr(line);
+
+            // Creating a new process
+            if (func == "P")
+            {
+                startNewProcess(split, line);
+            }
+            if (func != "#" && func != "P")
+            {
+                Console.Write("// TODO: ");
+                Console.WriteLine(line);
+            }
         }
-        private static void AlternatePathOfExecution()
+        public static string getSolutionPath()
         {
-            Console.WriteLine("Write something different on other Console");
-            Console.ReadLine();
+            string ret = "";
+            int contador = 0;
+            int i = PATH.Length - 1;
+            while (contador < 5) 
+            {
+                if (PATH[i] == '\\')
+                {
+                    contador += 1;
+                }
+                i--;
+            }
+            int j = 0;
+
+            while (j <= i) 
+            {
+                ret += PATH[j];
+                j++;
+            }
+            return ret;
         }
 
+        public static void Main(string[] args)
+        {
+            // OPEN CONFIG SCRIPT
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string relativePath = @"..\..\..\configFiles\configuration_sample.txt";
+            string filePath = Path.Combine(baseDirectory, relativePath);
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lineBehaviour(line);
+                }
+            }
+        }
     }
 }
